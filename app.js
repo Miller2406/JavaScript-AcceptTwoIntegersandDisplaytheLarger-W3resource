a2126de335b39c045deb903e24ca9f67a8f98644
Miller2406 <140726735+Miller2406@users.noreply.github.com>
2023-11-14 11:40:02 +0700

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+//Write a JavaScript program that displays the largest integer among two integers.
+
+let num1 = prompt(`input first number`)
+let num2 = prompt(`input second number`)
+alert(`Comparison is completed, please take a look in console`)
+
+function compareNums(num1, num2) {
+    if (num1 > num2) {
+        console.log(`number : ${num1} is greater than ${num2}`);
+    } else
+        if (num1 < num2) {
+            console.log(`number : ${num2} is greater than ${num1}`);
+        } else {
+            console.log(`number : ${num} and ${num2} is equal!`);
+        }
+}
+
+compareNums(num1, num2)
